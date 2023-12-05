6cdbf8b4a6be8c09c1184ba596f0e2318efac199
Mesaab <jeanne.cathe@etudiant.univ-reims.fr>
2023-12-05 18:15:59 +0100

    Project2 start

---
+++ proj2.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#----------Import and adjust dataset----------
+
+data = pd.read_csv('london_weather.csv')
+
+#Convert date column to datetime
+data['date'] = pd.to_datetime(data['date'], format='%Y%m%d')
+print(data.head(10))
+#Check the date conversion went well
+print(data['date'].min())
+print(data['date'].max())
+
+#Get yearly values
+year_avg = data.groupby(data['date'].dt.year).mean()
+print(year_avg.head(10))
+
+
+#----------Visualize dataset----------
+
+#Temperature
+plt.figure(figsize=(8, 5))
+plt.title('Evolution of temperature through the years in London')
+plt.plot(year_avg.index, year_avg['max_temp'], label='Max Temperature')
+plt.plot(year_avg.index, year_avg['mean_temp'], label='Mean Temperature')
+plt.plot(year_avg.index, year_avg['min_temp'], label='Min Temperature')
+plt.xlabel('Year')
+plt.ylabel('Temperature')
+plt.legend()
+plt.show()
+
+#Rain - per year
+plt.figure(figsize=(8, 5))
+plt.bar(year_avg.index, year_avg['precipitation'], color='blue', alpha=0.7)
+plt.title('Average rain per year in London')
+plt.xlabel('Year')
+plt.ylabel('Precipitation')
+plt.show()
+
+#Since figure shows us rainiest year was 2014 :
+#Avg rain per month in 2014
+
+#Get monthly values for comparison
+data_2014 = data[(data['date'].dt.year == 2014)]
+month_avg = data_2014.groupby(data['date'].dt.month).mean()
+
+#Rain - per month (2014)
+plt.figure(figsize=(8, 5))
+plt.bar(month_avg.index, month_avg['precipitation'], color='blue', alpha=0.7)
+plt.title('Average rain per month in 2014 in London')
+plt.xlabel('Month')
+plt.ylabel('Precipitation')
+plt.show()
